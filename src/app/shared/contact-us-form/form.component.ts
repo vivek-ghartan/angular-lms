44babde81959b5ce7086fbc83ba2fdb6488842cf
Yash Vibhandik <vibhandikyash@gmail.com>
2020-05-26 22:18:49 +0530

    Replaced Lorem Ipsum to original data from PDF

--- src/app/shared/contact-us-form/form.component.ts
+++ src/app/shared/contact-us-form/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-form',
+  selector: 'app-contact-us-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
